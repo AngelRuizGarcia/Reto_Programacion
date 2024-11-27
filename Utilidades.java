837f2cbf4a312aa5511990c79ec1d07a889beeb0
Alex Rovira <alerovmar@alu.edu.gva.es>
2024-11-27 17:25:05 +0100

    27/11 modificacion alejandro

--- Utilidades.java
+++ Utilidades.java
@@ -1,3 +1,20 @@
 public class Utilidades{
     
+    protected void textoPrincipio (){
+        System.out.println("Bienvenido al programa del tono la ines ");
+        System.out.println();
+        System.out.print("Selecione una opción:");
+        System.out.println();
+    }
+    protected void textoPrincipal(){
+        System.out.println("1.Panes");
+        System.out.println("2.Pizzas");
+        System.out.println("3.Pasteles");
+        System.out.println("4.Bebidas");
+        System.out.println("5.Finalizar compra:");
+        System.out.print("Introduce una opción: ");
+        
+    }
+   
+    
 }
